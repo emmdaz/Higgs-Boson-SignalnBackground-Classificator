feb96e6c9ab3d32e993c70a71487e0d1b56adca3
emmdaz <emmanuel.cruzd@alumno.buap.mx>
2026-04-10 05:08:37 +0000

    Cambios en el código para trabajar con antipartículas.

--- distribution_graph.py
+++ distribution_graph.py
@@ -21,12 +21,25 @@ def dist(file, PID = False, bins = 50, use_abs = False, name = False):
         phi_p = [phi[ind[i][0]][ind[i][1]] for i in range(len(ind))]
         charge_p = [charge[ind[i][0]][ind[i][1]] for i in range(len(ind))]
     else:
-        ind = [[i, l] for i in range(events) for l in np.where(pid[i] == PID)[0]]
-        pt_p = [pt[ind[i][0]][ind[i][1]] for i in range(len(ind))]
-        eta_p = [eta[ind[i][0]][ind[i][1]] for i in range(len(ind))]
-        phi_p = [phi[ind[i][0]][ind[i][1]] for i in range(len(ind))]
-        charge_p = [charge[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+        if PID >= 0:
+            ind = [[i, l] for i in range(events) for l in np.where(pid[i] == PID)[0]]
+            pt_p = [pt[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+            eta_p = [eta[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+            phi_p = [phi[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+            charge_p = [charge[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+        else:
+            ind = [[i, l] for i in range(events) for l in np.where(pid[i] == PID)[0]]
+            pt_t = [pt[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+            eta_t = [eta[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+            phi_t = [phi[ind[i][0]][ind[i][1]] for i in range(len(ind))]
+            charge_t = [charge[ind[i][0]][ind[i][1]] for i in range(len(ind))]
 
+            ind2 = [i for i, x in enumerate(pt_t) if (x > 0 and x < 1e6)]
+            pt_p = [pt_t[ind2[i]] for i in range(len(ind2))]
+            eta_p = [eta_t[ind2[i]] for i in range(len(ind2))]
+            phi_p = [phi_t[ind2[i]] for i in range(len(ind2))]
+            charge_p = [charge_t[ind2[i]] for i in range(len(ind2))]
+            
     if name:
         data = {
             "P_t" + " "*1 + name: pt_p,
